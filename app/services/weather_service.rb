6197fefb8d07615d9886b6c810a03f95ef810dfc
Brian Hayes <Brian.hayes1200@gmail.com>
2023-04-24 01:12:24 -0600

    Feat: set up weather service skeleton

---
+++ app/services/weather_service.rb
@@ -0,0 +1,21 @@
+class WeatherService
+  def self.get_five_day_forecast(params)
+   get_url("/forecast.json?#{params}&days=5")
+ end
+
+
+ def self.get_url(url)
+   response = conn.get(url)
+   JSON.parse(response.body, symbolize_names: true)
+ end
+
+
+ private
+
+
+ def self.conn
+   Faraday.new(url: "http://api.weatherapi.com/v1" ) do |farady|
+     faraday.headers["key"] = ENV["WEATHER_API_KEY"]
+   end
+ end
+end
