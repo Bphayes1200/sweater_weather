0e3decac5cfd9177d37a28754b52e2f2d23b0f8a
Brian Hayes <Brian.hayes1200@gmail.com>
2023-04-25 14:10:40 -0600

    Feat: add functionality to post a new session

---
+++ spec/requests/api/v1/session_request_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe "session requests" do 
+  it "can post a new session to the database" do 
+    new_user_data = { 
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+      }
+
+    new_user_headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/users", headers: new_user_headers , params: JSON.generate(new_user_data)
+    
+    user_data = { 
+      "email": "whatever@example.com",
+      "password": "password",
+      }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/sessions", headers: headers , params: JSON.generate(user_data)
+
+    expect(response.status).to eq(200)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:data][:id]).to_not eq(nil)
+    expect(data[:data][:attributes][:email]).to eq("whatever@example.com")
+    expect(data[:data][:attributes][:password]).to eq(nil)
+    expect(data[:data][:attributes][:api_key].length).to eq(32)
+  end
+end
